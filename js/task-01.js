992bb9f6ddbc49fe5cd94361d71f6e73fde1075f
LiudmilaSnizhko <128932644+LiudmilaSnizhko@users.noreply.github.com>
2023-07-26 00:28:59 +0300

    fix-task-01

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+// const item = document.querySelector('li')
+// console.log(item)
+const categories = document.querySelectorAll('ul');
+categories.forEach((category) => {
+    console.log(category);
+
+});
