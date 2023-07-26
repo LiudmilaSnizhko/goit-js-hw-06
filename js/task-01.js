7f87ca2b8fec77f57aac5cb37d85bf3f03258a4b
LiudmilaSnizhko <128932644+LiudmilaSnizhko@users.noreply.github.com>
2023-07-26 23:53:20 +0300

    task-01-fix

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,15 @@
-// const item = document.querySelector('li')
-// console.log(item)
-const categories = document.querySelectorAll('ul');
-categories.forEach((category) => {
-    console.log(category);
 
-});
+const categories = document.querySelectorAll('.item');
+console.log(`Number of categories:`, categories.length);
+
+categories.forEach((category) => {
+
+    const text = category.querySelector("h2").textContent;
+    console.log(`Category: ${text}`);
+
+    const list = category.querySelectorAll("li");
+    // const element = list.length;
+
+    console.log(`Elements: ${list.length}`);
+    
+   });
